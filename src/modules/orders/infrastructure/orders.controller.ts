b968a089855bcb55ccb809d1d81cfb86513d09d8
NandosJake <39186454+NandosJake@users.noreply.github.com>
2021-07-15 17:01:36 +0100

    feat: added orders controller

---
+++ src/modules/orders/infrastructure/orders.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { getPackBreakdown } from '../usecases/get-pack-breakdown.usecase';
+
+class OrderController {
+  async getPackBreakdown(req: Request, res: Response, next: NextFunction) {
+    try {
+      const packsGenerated = getPackBreakdown.execute({ orderQuantity: Number(req.params.orderquantity) });
+      res.json(packsGenerated);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+const orderController = new OrderController();
+
+export { orderController };
